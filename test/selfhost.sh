0fc57d7574ec4ec682b1dcbe30da99af96a1d7d3
oooooba <fuumin.23@gmail.com>
2019-08-17 13:02:22 +0900

    test: fix test harness

--- test/selfhost.sh
+++ test/selfhost.sh
@@ -1,63 +1,82 @@
 #!/bin/bash
 set -eu
 
-CC=../build/cccc
-CC2=cccc
+CCCC1=../build/cccc
+CCCC2=./cccc2
+CCCC3=cccc3
 GCC=gcc
 SRC=..
 INCLUDE=../test_include
 TMP=/tmp
 
 function compile_cccc () {
-    $GCC -std=c11 -Wall -I$INCLUDE -E $SRC/$1.c >$TMP/$1.c
-    $CC <$TMP/$1.c >$TMP/$1.S
+    src=$SRC/$2.c
+    tmp=$TMP/$2_$1.c
+    asm=$TMP/$2_$1.S
+
+    $GCC -std=c11 -Wall -I$INCLUDE -E $src >$tmp
+    $CCCC <$tmp >$asm
 }
 
 function compile_gcc () {
-    $GCC -std=c11 -Wall $SRC/$1.c -S -o $TMP/$1.S
+    src=$SRC/$2.c
+    asm=$TMP/$2_$1.S
+    $GCC -std=c11 -Wall $src -S -o $asm
 }
 
-compile_cccc main
-compile_cccc codegen
-compile_cccc context
-compile_cccc dump
-compile_cccc fixup
-compile_cccc ir
-compile_cccc lexer
-compile_cccc list
-compile_cccc map
-compile_cccc nameresolve
-compile_cccc parser
-compile_cccc regalloc
-compile_cccc strtable
-compile_cccc simplify
-compile_cccc type
-compile_cccc typing
-compile_cccc vector
-compile_cccc visitor
+function link () {
+    gen=$1
+    compiler=cccc$((gen+1))
+    $GCC -std=c11 -Wall \
+        $TMP/main_$gen.S \
+        $TMP/codegen_$gen.S \
+        $TMP/context_$gen.S \
+        $TMP/dump_$gen.S \
+        $TMP/fixup_$gen.S \
+        $TMP/ir_$gen.S \
+        $TMP/lexer_$gen.S \
+        $TMP/list_$gen.S \
+        $TMP/map_$gen.S \
+        $TMP/nameresolve_$gen.S \
+        $TMP/parser_$gen.S \
+        $TMP/regalloc_$gen.S \
+        $TMP/strtable_$gen.S \
+        $TMP/simplify_$gen.S \
+        $TMP/type_$gen.S \
+        $TMP/typing_$gen.S \
+        $TMP/vector_$gen.S \
+        $TMP/visitor_$gen.S \
+        -o $compiler
+}
 
-$GCC -std=c11 -Wall \
-    $TMP/main.S \
-    $TMP/codegen.S \
-    $TMP/context.S \
-    $TMP/dump.S \
-    $TMP/fixup.S \
-    $TMP/ir.S \
-    $TMP/lexer.S \
-    $TMP/list.S \
-    $TMP/map.S \
-    $TMP/nameresolve.S \
-    $TMP/parser.S \
-    $TMP/regalloc.S \
-    $TMP/strtable.S \
-    $TMP/simplify.S \
-    $TMP/type.S \
-    $TMP/typing.S \
-    $TMP/vector.S \
-    $TMP/visitor.S \
-    -o $CC2
+# compile with 1st-generation compiler
+
+CCCC=$CCCC1
+
+compile_gcc 1 main
+compile_gcc 1 codegen
+compile_gcc 1 context
+compile_gcc 1 dump
+compile_gcc 1 fixup
+compile_gcc 1 ir
+compile_gcc 1 lexer
+compile_gcc 1 list
+compile_gcc 1 map
+compile_gcc 1 nameresolve
+compile_gcc 1 parser
+compile_gcc 1 regalloc
+compile_gcc 1 strtable
+compile_gcc 1 simplify
+compile_gcc 1 type
+compile_gcc 1 typing
+compile_gcc 1 vector
+compile_gcc 1 visitor
+
+link 1
+
+# test 2nd-generation compiler
 
 $GCC -std=c11 -Wall -I$INCLUDE -E test.c >$TMP/test.c
-$CC <$TMP/test.c >/tmp/output.S
+$CCCC2 <$TMP/test.c >/tmp/output.S
 $GCC -std=c11 -Wall main.c /tmp/output.S
 ./a.out
